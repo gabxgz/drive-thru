4a1c280a8ccc6df23a3286465aa76aab6af2efcc
GeorgeGabe <gabegonzalez24@gmail.com>
2017-07-14 20:14:39 -0500

    yarn server setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPluginConfig = new HtmlWebPackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
 module.exports = {
   entry: './client/index.js',
   output: {
@@ -12,4 +19,5 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
     ],
   },
+  plugins: [HtmlWebPackPluginConfig],
 }
